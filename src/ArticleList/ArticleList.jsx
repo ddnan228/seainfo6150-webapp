85cbe75cb885a09639e3cbaf0f15d554173ea4b4
Doudou Nan <ddnan228@gmail.com>
2020-10-25 12:35:29 -0700

    css assignment

--- src/ArticleList/ArticleList.jsx
+++ src/ArticleList/ArticleList.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ArticleListItem from "../ArticleListItem/ArticleListItem.jsx"
 import PropTypes from "prop-types"
+import css from "./ArticleList.module.css"
 
 const ArticleList = props => {
   return (
-    <ul>
+    <ul className = {css.articleList}>
       {props.articles.map(article => (
         <li key={article.slug}>
           <ArticleListItem article ={article} />
--- src/ArticleListItem/ArticleListItem.jsx
+++ src/ArticleListItem/ArticleListItem.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import HTMLText from "../HTMLText/HTMLText";
+import css from "./ArticleListItem.module.css"
+import ArticleTextToggleButton from "../ArticleTextToggleButton/ArticleTextToggleButton";
 
 const ArticleListItem = (props) => {
+  const [buttonText, setText] = useState("Show more");
+  const onClick = useCallback(
+    event => {
+      event.preventDefault();
+      setText(buttonText === "Show more" ? "Show less" : "Show more");
+    },
+    [buttonText]
+  );
+
+  let content;
+  if (buttonText === "Show less") {
+    content = <div>
+                <p>{props.article.shortText}</p>
+                <p className = {css.time}>{props.article.displayDate}</p>
+              </div>;
+  } else {
+    content = "";
+  }
+
   return (
-    <article>
-        <h1>{props.article.title}</h1>
-        <p>{props.article.displayDate} {props.article.pubYear}<br /></p>
-        <p>{props.article.shortText}</p>
+    <article className = {css.articleBody}>
+        <h1 className = {css.articleTitle}>{props.article.title}</h1>
+        {content}
+        <ArticleTextToggleButton  buttonText = {buttonText} onClick = {onClick} />
     </article>
   );
 };
